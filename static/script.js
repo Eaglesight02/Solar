959e3021d1b90dd5f29997551ed43c58a3fc84f7
ypavan2023 <pavan98482023@gmail.com>
2024-02-06 19:00:49 +0000

    7-2-24 12:30am 1

--- static/script.js
+++ static/script.js
@@ -25,3 +25,23 @@ inputFile.addEventListener('change', function () {
 		alert("Image size more than 2MB");
 	}
 })
+
+var acc = document.getElementsByClassName("accordion");
+      var i;
+
+      for (i = 0; i < acc.length; i++) {
+        acc[i].addEventListener("click", function () {
+          this.classList.toggle("active");
+          this.parentElement.classList.toggle("active");
+
+          var pannel = this.nextElementSibling;
+
+          if (pannel.style.display === "block") {
+            pannel.style.display = "none";
+          } else {
+            pannel.style.display = "block";
+          }
+        });
+      }
+
+
